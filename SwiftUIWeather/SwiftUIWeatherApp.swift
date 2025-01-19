1f1904339d2e5b75917a42693042f1f03c2b6005
haydgraham <samuelh.graham@gmail.com>
2025-01-19 14:29:12 -0700

    Initial Commit

---
+++ SwiftUIWeather/SwiftUIWeatherApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIWeatherApp.swift
+//  SwiftUIWeather
+//
+//  Created by Samuel Hayden Graham on 1/19/25.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIWeatherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ SwiftUIWeatherTests/SwiftUIWeatherTests.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIWeatherTests.swift
+//  SwiftUIWeatherTests
+//
+//  Created by Samuel Hayden Graham on 1/19/25.
+//
+
+import Testing
+@testable import SwiftUIWeather
+
+struct SwiftUIWeatherTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}

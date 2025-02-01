3b0220e40f78a9a1ebcee673b55db6e2f17cb794
haydgraham <samuelh.graham@gmail.com>
2025-02-01 10:48:25 -0700

    first commit

---
+++ SwiftUIWeather/WeatherButton.swift
@@ -0,0 +1,18 @@
+//
+//  WeatherButton.swift
+//  SwiftUIWeather
+//
+//  Created by Samuel Hayden Graham on 2/1/25.
+//
+
+import SwiftUI
+
+struct WeatherButton: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    WeatherButton()
+}
